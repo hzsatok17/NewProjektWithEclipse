1956c0a4a643d1df5b3f7990f2114efc219e51af
unknown <Zsolt_Horvath2@EPHUSZEW0297.budapest.epam.com>
2015-10-01 19:04:36 +0200

    s

---
+++ test/src/main/java/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		writeOHBoyoMessageOnConsole();
+	
+	}
+
+	public static void writeOHBoyoMessageOnConsole(){
+		System.out.println("Oh yeh boyo");
+
+	}
+}
